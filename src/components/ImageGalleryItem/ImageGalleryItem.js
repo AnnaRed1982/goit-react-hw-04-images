5c5eedbedbe450cb9d36588d4ee60d2d367fe383
annared <annared1982@gmail.com>
2023-03-08 12:44:43 +0200

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,35 +1,30 @@
 import css from './ImageGalleryItem.module.css';
-import { Component } from 'react';
+import { useState } from 'react';
 import { Modal } from '../Modal/Modal';
 import PropTypes from 'prop-types';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ webformatURL, tags, largeImageURL }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const modalToggle = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
-  modalToggle = () => {
-    this.setState({ isModalOpen: !this.state.isModalOpen });
-  };
+  return (
+    <li className={css.imageGalleryItem}>
+      <img
+        src={webformatURL}
+        alt={tags}
+        className={css['imageGalleryItem-image']}
+        onClick={modalToggle}
+      />
+      {isModalOpen && (
+        <Modal largeImageURL={largeImageURL} onClose={modalToggle} />
+      )}
+    </li>
+  );
+};
 
-  render() {
-    const { webformatURL, tags, largeImageURL } = this.props;
-
-    return (
-      <li className={css.imageGalleryItem}>
-        <img
-          src={webformatURL}
-          alt={tags}
-          className={css['imageGalleryItem-image']}
-          onClick={this.modalToggle}
-        />
-        {this.state.isModalOpen && (
-          <Modal largeImageURL={largeImageURL} onClose={this.modalToggle} />
-        )}
-      </li>
-    );
-  }
-}
 ImageGalleryItem.propTypes = {
   id: PropTypes.string,
   webformatURL: PropTypes.string,
