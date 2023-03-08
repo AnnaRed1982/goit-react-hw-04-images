e74949b82e35ce39ed2bc63fae4ab9ee6a20a026
annared <annared1982@gmail.com>
2023-03-08 14:50:20 +0200

    app hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,75 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchSearchImage } from '../services/api';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 
-export class App extends Component {
-  state = {
-    serchRequest: '',
-    images: [],
-    error: null,
-    status: 'ideal',
-    page: 1,
-  };
+export const App = () => {
+  const [serchRequest, setSerchRequest] = useState('');
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('ideal');
+  const [page, setPage] = useState('1');
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.serchRequest !== this.state.serchRequest ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ status: 'pending' });
-      try {
-        const response = await fetchSearchImage(
-          this.state.serchRequest,
-          this.state.page
+  useEffect(() => {
+    if (serchRequest === '') {
+      return;
+    }
+    setStatus('pending');
+
+    const fetchResponse = async () => {
+      const response = await fetchSearchImage(serchRequest, page);
+
+      if (response.length === 0) {
+        setStatus(null);
+        return alert(
+          'Sorry, there are no images matching your search query. Please try again.'
         );
-        if (response.length === 0) {
-          this.setState({ status: null });
-          return alert(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        }
-        if (response.length > 0) {
-          this.setState(({ images }) => ({
-            images: [...images, ...response],
-            status: 'resolved',
-          }));
-        }
-      } catch (error) {
-        this.setState({ status: 'rejected', error });
       }
+      if (response.length > 0) {
+        setImages(prevImages => [...prevImages, ...response]);
+        setStatus('resolved');
+      }
+    };
+
+    try {
+      fetchResponse();
+    } catch (error) {
+      setStatus('rejected');
+      setError(error);
     }
+  }, [serchRequest, page]);
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleFormSubmit = serchRequest => {
+    setSerchRequest(serchRequest);
+    setPage(1);
+    setImages([]);
+  };
+
+  if (status === 'pending') {
+    return <Loader />;
   }
-
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  handleFormSubmit = serchRequest => {
-    this.setState({ serchRequest, page: 1, images: [] });
-  };
-
-  render() {
-    const { status, error, images } = this.state;
-
-    if (status === 'pending') {
-      return <Loader />;
-    }
-    if (status === 'rejected') {
-      return <h2>Whoops, something went wrong: {error.message}</h2>;
-    }
-    if (status === 'resolved') {
-      return (
-        <div className="app">
-          <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery images={images} />
-          <Button onClick={this.loadMore} />
-        </div>
-      );
-    }
+  if (status === 'rejected') {
+    return <h2>Whoops, something went wrong: {error.message}</h2>;
+  }
+  if (status === 'resolved') {
     return (
       <div className="app">
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={handleFormSubmit} />
+        <ImageGallery images={images} />
+        <Button onClick={loadMore} />
       </div>
     );
   }
-}
+  return (
+    <div className="app">
+      <Searchbar onSubmit={handleFormSubmit} />
+    </div>
+  );
+};
+
