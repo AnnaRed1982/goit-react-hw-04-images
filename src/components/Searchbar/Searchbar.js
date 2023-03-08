13ba6559491e2e2f6bb1d021aab03647e2739b97
annared <annared1982@gmail.com>
2023-03-08 11:56:38 +0200

    hook search bar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,51 +1,49 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './Searchbar.module.css';
 import { RxMagnifyingGlass } from 'react-icons/rx';
 import PropTypes from 'prop-types';
 
-export class Searchbar extends Component {
-  state = { serchRequest: '' };
+export const Searchbar = ({ onSubmit }) => {
+  const [serchRequest, setSerchRequest] = useState('');
 
-  handleSerchRequestChange = e => {
-    this.setState({ serchRequest: e.currentTarget.value.toLowerCase() });
+  const handleSerchRequestChange = e => {
+    setSerchRequest(e.currentTarget.value.toLowerCase());
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.serchRequest.trim() === '') {
+    if (serchRequest.trim() === '') {
       return alert('Please fill search request!');
     }
 
-    this.props.onSubmit(this.state.serchRequest);
-    this.setState({ serchRequest: '' });
-    // e.currentTarget.reset();
+    onSubmit(serchRequest);
+    setSerchRequest('');
   };
 
-  render() {
-    return (
-      <header className={css.searchbar}>
-        <form className={css.searchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css['searchForm-button']}>
-            <RxMagnifyingGlass size="30" />
-            <span className={css['searchForm-button-label']}>Search</span>
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className={css.searchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css['searchForm-button']}>
+          <RxMagnifyingGlass size="30" />
+          <span className={css['searchForm-button-label']}>Search</span>
+        </button>
 
-          <input
-            className={css['searchForm-input']}
-            type="text"
-            name="serchRequest"
-            value={this.state.serchRequest}
-            onChange={this.handleSerchRequestChange}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css['searchForm-input']}
+          type="text"
+          name="serchRequest"
+          value={serchRequest}
+          onChange={handleSerchRequestChange}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
 };
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const API_KEY = '32381232-0d08b52c11723d23aba771294';
 
-export const fetchSearchImage = async (serchRequest, page) => {
+export const fetchSearchImage = async (serchRequest, page=1) => {
   const response = await axios.get(
     `https://pixabay.com/api/?q=${serchRequest}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12&`
   );
